19d3b9817bcf0909c692f895a784b531c8086b16
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-20 09:21:32 +0000

    php

---
+++ array.php
@@ -0,0 +1,9 @@
+<?php
+$intArray = arr(10,20,40);
+echo "First element : $intArray[0],<br>";
+echo "Second element : $intArray[1],<br>";
+echo "Third element : $intArray[2],<br>";
+
+var_dump($intArray);
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,52 @@
 
 <?php
-echo "My first PHP script!";
+echo "My first PHP script! \n";
+$x = 5;
+var_dump($x);
+?> 
+
+
+
+<?php
+
+var_dump("Nilesh");
+echo"<br>"
+
 ?>
+
+<?php
+// This is a single-line comment
+echo "Hello, World!";
+
+/* 
+ This is a multi-line comment
+ that spans multiple lines
+*/
+echo "Hello, again!";
+?>
+
+
+<?php
+$intArray = array(10,20,40);
+echo "First element : $intArray[0],<br>";
+echo "Second element : $intArray[1],<br>";
+echo "Third element : $intArray[2],<br>";
+
+var_dump($intArray);
+
+?>
+
+
+
+
+<?php
+$Fruits = ['apple', 'banana', 'Orange'];
+echo($Fruits[0]);
+
+$person = ['name '=> 'John','age' =>30, 'location ' => 'new york'];
+
+echo$person['name'];
+
+?>
+
+
