cb9cd07b1d7260e33a80be4acabe52c1c024891b
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-22 09:46:59 +0000

    phhp

---
+++ variables.php
@@ -0,0 +1,21 @@
+<?php
+$name = "Nilesh kumar";
+echo $name;
+echo "<br>";
+echo "Hello {$name}";
+echo "<br>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Submit</button>
+    
+</body>
+</html>
