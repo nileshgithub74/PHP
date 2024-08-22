51f2ad98d0d14443f3c61fbcf985b2d6921c5655
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-22 09:29:49 +0000

    php

--- Operator.php
+++ Operator.php
@@ -1,7 +1,3 @@
-<!-- 1. arithetic Operator
- 2. Assignment Operator
- 3. Comparison Operatot
- 4. Increment & Decrement Operator  -->
 
  <?php
 //     $X = 10;
@@ -27,7 +23,7 @@
 
 
 
-//  Increment and Decrement Operator
+//==================================================  Increment and Decrement Operator
 
 // $x = 10;
 // echo "The value of B after increment : ";
@@ -43,29 +39,70 @@
 // echo"<br>";   
 
 
-// String Operartor =========
+// =========================================================String Operartor =========
 
 
-$x = "Hello ";
-$y =  "world ";
+// $x = "Hello ";
+// $y =  "world ";
 
-echo ($x.$y);
-echo "<br>";
-
-
-// Condtional  statement 
-
-$marks = 80;
-
-echo ($marks <60)  ? 'B Grade' : 'A grade';
+// echo ($x.$y);
+// echo "<br>";
 
 
+// =========================================================Condtional  statement============= 
+
+// $marks = 80;
+
+// echo ($marks <60)  ? 'B Grade' : 'A grade';
+
+// echo "enter the name";
+// $age = readline();
+
+// if($age >= 18){
+//     echo "You are adult.";
+
+// }else{
+//     echo "You are a minor";
+// }
+
+
+// echo "enter the value of marks";
+// $mark = readline();
+
+// if($mark >= 80){
+//     echo " A grade";
+// }else if($mark >= 60 && $mark <80){
+//     echo "B grade";
+// }else if($mark >=40 && $mark <60){
+//     echo " C grade";
+// }else{
+//     echo " you are failed";
+// }
+
+// echo "Enter the year";
+// $year = readline();
+
+// if(($year % 400 ==0 ) || ($year% 100 != 0) && ($year % 4==0)){
+//     echo " it is leap year";
+
+// }else{
+//     echo " it is not a leap year";
+// }
+
+
+// $a = array("a" => "apple", "b" => "banana");
+// $b = array("a" => "pear", "b" => "strawberry", "c" => "cherry");
+
+// $c = $a + $b;  // Union of $a and $b
+// print_r($c);   // Outputs: Array ( [a] => apple [b] => banana [c] => cherry )
 
 
 
+// $a = array("apple", "banana");
+// $b = array(1 => "banana", 0 => "apple");
 
-
-
+// var_dump($a == $b);
+// var_dump($a === $b);
 
 
 ?>
