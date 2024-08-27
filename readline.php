ecdf50bc3fd0d34f1b860b3a12de682512f31595
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-27 10:13:30 +0000

    php

---
+++ readline.php
@@ -0,0 +1,24 @@
+<?php
+
+$num1 = readline("Enter the first number: ");
+$num2 = readline("Enter the second number: ");
+
+
+
+$num1 = (int)$num1;
+$num2 = (int)$num2;
+
+
+$sum = $num1 + $num2;
+$difference = $num1 - $num2;
+$product = $num1 * $num2;
+$quotient = $num1 / $num2;
+
+
+
+
+echo "Sum: $sum\n";
+echo "Difference: $difference\n";
+echo "Product: $product\n";
+echo "Quotient: $quotient\n";
+?>
