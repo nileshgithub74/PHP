d98b9cf4bb1d35ccd9ef14e2bd8777ca00fc9ad7
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-27 09:27:28 +0000

    php

--- Conditional.php
+++ Conditional.php
@@ -31,24 +31,46 @@
 //---------------------check the prime number  
 
 
-function is_prime($number) {
-    if ($number <= 1) {
-        return false;
-    }
-    for ($i = 2; $i <= $number / 2; $i++) {
-        if ($number % $i == 0) {
-            return false;
-        }
-    }
-    return true;
+// function is_prime($number) {
+//     if ($number <= 1) {
+//         return false;
+//     }
+//     for ($i = 2; $i <= $number / 2; $i++) {
+//         if ($number % $i == 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// $number = 29;
+// if (is_prime($number)) {
+//     echo "$number is a prime number";
+// } else {
+//     echo "$number is not a prime number";
+// }
+
+
+
+$marks = 85; // Example marks
+
+if ($marks >= 90) {
+    $grade = "A+";
+} elseif ($marks >= 80) {
+    $grade = "A";
+} elseif ($marks >= 70) {
+    $grade = "B";
+} elseif ($marks >= 60) {
+    $grade = "C";
+} elseif ($marks >= 50) {
+    $grade = "D";
+} else {
+    $grade = "F";
 }
 
-$number = 29;
-if (is_prime($number)) {
-    echo "$number is a prime number";
-} else {
-    echo "$number is not a prime number";
-}
+echo "The grade for marks $marks is: $grade";
+
+
 
 
 
