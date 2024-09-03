5488d8628708c2a9c31ca4ac49701bab0daafa38
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-09-03 09:45:36 +0000

    php

--- readline.php
+++ readline.php
@@ -42,24 +42,33 @@
 <?php
 
 
-$size = (int)readline("Enter the size of the array: ");
-$myArray = [];
-for($i = 0; $i < $size; $i++) {
-    $myArray[$i] = (int)readline("Enter element " . ($i + 1) . ": ");
+// $size = (int)readline("Enter the size of the array: ");
+// $myArray = [];
+// for($i = 0; $i < $size; $i++) {
+//     $myArray[$i] = (int)readline("Enter element " . ($i + 1) . ": ");
+// }
+
+// $sum = 0;
+// $even = 0;
+// for($i = 0; $i < $size; $i++) {
+//     $sum += $myArray[$i];
+//     if($myArray[$i] % 2 == 0) {
+//         $even++;
+//     }
+// }
+
+// $average = $sum / $size;
+// print_r("Average: " . $average . "\n");
+// print_r("Number of even elements: " . $even . "\n");
+
+
+?>
+
+
+
+
+<?php
+for ($i = 1; $i <= 5; $i++) {
+    echo "Number: $i\n";
 }
-
-$sum = 0;
-$even = 0;
-for($i = 0; $i < $size; $i++) {
-    $sum += $myArray[$i];
-    if($myArray[$i] % 2 == 0) {
-        $even++;
-    }
-}
-
-$average = $sum / $size;
-print_r("Average: " . $average . "\n");
-print_r("Number of even elements: " . $even . "\n");
-
-
 ?>
