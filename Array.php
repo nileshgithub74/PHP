8b74d31867d062688993fa9a127b0bdb1efb784a
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-09-05 09:18:24 +0000

    php

---
+++ Array.php
@@ -0,0 +1,25 @@
+<?php
+// $cars = array("Volvo", "BMW", "Toyota");
+
+// print_r($cars);
+
+
+
+// $age = array("Peter" => 35, "Ben" => 37, "Joe" => 43);
+// print_r($age);
+
+
+
+
+ ?>
+
+
+<?php
+$cars = array(
+    array("Volvo", 22, 18),
+    array("BMW", 15, 13),
+    array("Saab", 5, 2),
+    array("Land Rover", 17, 15)
+);
+print_r($cars);
+?>
