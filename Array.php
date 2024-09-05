f92f6735651432c3e6e59dbe5c928677bcea01d0
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-09-05 09:34:59 +0000

    php

--- Array.php
+++ Array.php
@@ -15,11 +15,50 @@
 
 
 <?php
-$cars = array(
-    array("Volvo", 22, 18),
-    array("BMW", 15, 13),
-    array("Saab", 5, 2),
-    array("Land Rover", 17, 15)
-);
-print_r($cars);
+// $cars = array(
+//     array("Volvo", 22, 18),
+//     array("BMW", 15, 13),
+//     array("Saab", 5, 2),
+//     array("Land Rover", 17, 15)
+// );
+// print_r($cars);
+
+
+
+
+
+
+// echo $cars[0]; 
+// echo $cars[1]; 
+// echo $cars[2]; 
+
+?>
+
+
+
+<?php
+// $cars = array("Volvo", "BMW", "Toyota");
+// $length = count($cars);
+
+// for ($i = 0; $i < $length; $i++) {
+//     echo $cars[$i] . "<br>";
+// }
+
+// $colors = array("red", "green", "blue", "yellow");
+
+// foreach ($colors as $color) {
+//     echo $color . "<br>";
+// }
+
+
+
+?>
+
+//===============Associative Array : 
+
+
+<?php
+
+$ courses = array("Int220 " => "php", "int221" =>"Laravel", "int221" =>"node js");
+
 ?>
