4da4a86423d55fc91f65fccc3cc0b2c635346ed5
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-09-12 10:53:30 +0000

    php

--- CommonArray.php
+++ CommonArray.php
@@ -21,9 +21,55 @@
 
 
 
-$input_array = array('a', 'b', 'c', 'd', 'e');
-print_r(array_chunk($input_array, 2));
-print_r(array_chunk($input_array, 2, true));
+// $input_array = array('a', 'b', 'c', 'd', 'e');
+// print_r(array_chunk($input_array, 2));
+
+
+// $input_array = array("A", "Cat", "Dog", "A", "Dog");
+// $result = array_count_values($input_array);
+// print_r($result);
+
+
+
+
+
+
+// $array1 = array("a" => "green", "red", "blue", "red");
+// $array2 = array("b" => "green", "yellow", "red");
+// $result = array_diff($array1, $array2);
+// print_r($result);
+
+
+
+// $input = array("a" => "red", "b" => "green", "c" => "blue");
+// $result = array_flip($input);
+// print_r($result);
+
+
+$array1 = array("a" => "green", "red", "blue");
+$array2 = array("b" => "green", "yellow", "red");
+$result = array_intersect($array1, $array2);
+print_r($result);
+
+
+
+
+
+$input = array("a" => "Volvo", "b" => "BMW", "c" => "Toyota");
+$result = array_reverse($input);
+print_r($result);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ?>
